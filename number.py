b144921b3c8c9c9e52eb4fee8dbbd9f5990e819e
spacecats01 <34550776+spacecats01@users.noreply.github.com>
2018-01-09 17:10:21 +0100

    Delete number.py

--- number.py
+++
@@ -1,9 +0,0 @@
-from random import randrange
-print ('<<<<<Kickass Dice Simulator>>>>>')
-odpoved = input ('Roll the dice \n')
-
-if odpoved == 'roll':
-    number = randrange (0, 100)
-    print ('You rolled', number)
-else:
-    print ('Type /roll/')
